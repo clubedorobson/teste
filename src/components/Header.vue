e5068575e1f3f4ff3928663f4ccec0be1e8f9680
clubedorobson <clubedorobson@gmail.com>
2021-01-10 15:23:18 -0300

    Color and fixed-top

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <a class="navbar-brand" href="#">Clube do Robson</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -35,3 +35,11 @@ export default {
   }
 }
 </script>
+
+<style>
+
+.fixed-top{
+    background-color: #bf00cc;
+}
+
+</style>
